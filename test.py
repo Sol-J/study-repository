2e14a336c1aa8027f657ce5180eacc82dbc3d15e
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-08-04 16:39:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -30,6 +30,11 @@ def get_book_list(path):
 def open_browser():
     options = webdriver.ChromeOptions() 
     options.add_argument('headless')
+    options.add_argument("disable-gpu") 
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    prefs = {'profile.default_content_setting_values': {'cookies' : 2, 'images': 2, 'plugins' : 2, 'popups': 2, 'geolocation': 2, 'notifications' : 2, 'auto_select_certificate': 2, 'fullscreen' : 2, 'mouselock' : 2, 'mixed_script': 2, 'media_stream' : 2, 'media_stream_mic' : 2, 'media_stream_camera': 2, 'protocol_handlers' : 2, 'ppapi_broker' : 2, 'automatic_downloads': 2, 'midi_sysex' : 2, 'push_messaging' : 2, 'ssl_cert_decisions': 2, 'metro_switch_to_desktop' : 2, 'protected_media_identifier': 2, 'app_banner': 2, 'site_engagement' : 2, 'durable_storage' : 2}}   
+    options.add_experimental_option('prefs', prefs)
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
     driver = webdriver.Chrome(executable_path='./chromedriver', options=options)
     return driver
