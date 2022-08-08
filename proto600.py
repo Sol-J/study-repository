51df2ffbd8376d558902c2e5df2ab3a91cc05eb9
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-08-08 21:24:44 +0900

    Update proto600.py

--- proto600.py
+++ proto600.py
@@ -7,5 +7,5 @@
 import proto
 from proto import *
 info_list = get_book_list("solbook.xlsx")
-publishers, book_names, inf_ids, inf_urls, inf_dates = get_infringe_naver_blog(info_list[300:6000])
+publishers, book_names, inf_ids, inf_urls, inf_dates = get_infringe_naver_blog(info_list[300:600])
 
