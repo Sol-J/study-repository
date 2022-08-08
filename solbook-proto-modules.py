eb2f43d027a182c50ed3376f9514ea6accdb6643
Sol-J <80877740+Sol-J@users.noreply.github.com>
2022-08-08 17:45:54 +0900

    Update solbook-proto-modules.py

--- solbook-proto-modules.py
+++ solbook-proto-modules.py
@@ -1,8 +1,27 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+import selenium
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+from datetime import datetime
+import multiprocessing
+from multiprocessing import Pool
 
+from bs4 import BeautifulSoup
+import requests
+
+import re
+import pandas as pd
+import csv
+import math
+from tqdm.auto import tqdm
 
 def get_book_list(path):
     df = pd.read_excel(path)
